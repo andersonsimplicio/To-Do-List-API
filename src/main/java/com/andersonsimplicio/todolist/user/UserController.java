579d737b1c61e34fdaf6081e2b329f313b9adedb
andersonsimplicio <anderson25eu@gmail.com>
2026-09-23 00:38:02 -0300

    validar username

--- src/main/java/com/andersonsimplicio/todolist/user/UserController.java
+++ src/main/java/com/andersonsimplicio/todolist/user/UserController.java
@@ -3,15 +3,21 @@ package com.andersonsimplicio.todolist.user;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController 
 @RequestMapping("/users")
 public class UserController {
-    
-     @PostMapping
-    public void created(@RequestBody UserModel user){
-        System.out.println("Nome: "+user.getName() +" "+ user.getUserName());
+    @Autowired 
+    private UserRepository userRepository;
+
+    @PostMapping
+    public UserModel created(@RequestBody UserModel user){
+        System.out.println("Nome: "+user.getName() +" "+ user.getUsername());
+        var user_created = this.userRepository.save(user);
+        return user_created;
+
     }
 }
--- src/main/java/com/andersonsimplicio/todolist/user/UserModel.java
+++ src/main/java/com/andersonsimplicio/todolist/user/UserModel.java
@@ -1,8 +1,12 @@
 package com.andersonsimplicio.todolist.user;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -10,12 +14,16 @@ import lombok.Data;
 Usuando Lombok para gerar os getter e setter
 */
 @Data 
-@Entity
+@Entity(name="tb_users")
 public class UserModel {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String userName;
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(name="usuario")
+    private String username;
     private String name;
     private String password;
+    @CreationTimestamp 
+    private LocalDateTime createdAt;
 }
